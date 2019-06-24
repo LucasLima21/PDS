2ca2db2da22dbe53d122dc6629e620780a78efba
Lucas Lima <38709172+LucasLima21@users.noreply.github.com>
2019-06-24 12:20:41 -0400

    done

--- Project3-PDS.py
+++ Project3-PDS.py
@@ -6,97 +6,84 @@ import matplotlib.pyplot as plt
 from numpy import *
 from pylab import *
 
-
+def trade_X_to_Y(signalInput):
+  concatenation = ""
+  for i in range(len(signalInput)):
+    if(signalInput[i] == 'x'):
+      concatenation+="y"
+    else:
+        concatenation+=signalInput[i]
+  return concatenation
 
 def option1():#função que roda a opção 1 a qual recebe a equação do sinal analógico
-    print("Insira o sinal Analógico\n Exemplos do formato de entrada(sin(x)+cos(x), cos(2*x), 3*tan(2*x)), exp(x) e log(x)")
-    x = linspace(-2*pi, 2*pi, 50)
-    signal = eval(input())
-   
+    print("Insira o sinal Analogico\n Exemplos do formato de entrada(sin(x) + cos(x), cos(2*x) - 2*sin(x): ")
+    x = np.linspace(-2*pi, 2*pi, 60)
+    signalInput = input()
+    signalNormalFrequency = eval(signalInput)
+    #sinal analogico ja com o valor do periodo de amostragem
     
-    signal1 = []
-    x1 = []
-    for i in range(0,len(x),2):
-      x1.append(x[i])
-      signal1.append(signal[i])
-      
+    
+    #seguindo o teorema de amostragem de nyquist
+    #para que eu nao tenha aliasing
+    #preciso representar a frequencia de amostragem
+    #com o dobro da frequencia maxima do sinal !!
+    #como eu estabeleci uma período que todo sinal terá, o qual é de 2pi
+    #logo é só reduzir esse periodo, assim, duplicanddo a frequencia
+    y = np.linspace(-4*pi, 4*pi, 80)
+    signalInputTraded = trade_X_to_Y(signalInput)
+    signalDoubleFrequency = eval(signalInputTraded)
+    
+    
+    #print(x,"\n")
+    #print(signal)
     sample = x
-    sample1 = x1
+    sample1 = y
     
     
-    #Plot Analógico
-    plt.subplot(2,2,1)
+    
+    #Plot Analogico
+    plt.subplot(2,1,1)
     
     plt.subplots_adjust(hspace=0.6, wspace=0.6)
-    xticks([]), yticks([])
-    plt.title("Sinal Analógico")
+    
+    plt.title("Sinal Analogico")
     plt.axhline(0, color = 'black')
     plt.axvline(0, color = 'black')
     plt.ylabel('X[n]')
     plt.xlabel('n')
     plt.grid()
-    plt.plot(sample,signal,'-')
+    plt.plot(sample,signalNormalFrequency,'-', color='green')
     
 
 
-    #plot quantizado
-    plt.subplot(2,2,2)
-    plt.subplots_adjust(hspace=0.6, wspace=0.6)
-    xticks([]), yticks([])
-    plt.title("Sinal Quantizado")
-    plt.axhline(0, color = 'black')
-    plt.axvline(0, color = 'black')
-    plt.ylabel('X[n]')
-    plt.xlabel('n')
-    plt.grid()
-    xi = np.round(3 * signal)
-    xQ = 1/3 * xi
-    plt.plot(sample,xQ,'-')
-    
     
     #plot discretizado
-    plt.subplot(2,2,3)
+    plt.subplot(2,1,2)
     plt.subplots_adjust(hspace=0.6, wspace=0.6)
-    xticks([]), yticks([])
+    
     plt.title("Sinal Discretizado")
     plt.axhline(0, color = 'black')
     plt.axvline(0, color = 'black')
     plt.ylabel('X[n]')
     plt.xlabel('n')
     plt.grid()
-    for i in range(0,len(sample1)):
-      plt.stem(sample1,signal1)  
-      plt.plot(sample1[i],signal1[i],'o')
-    
+    #reduzindo pela metade o período, duplicando a frequencia!
+    plt.stem(sample1/2,signalDoubleFrequency)  
+   
 
     
-    #plot discretizado quantizado
-    plt.subplot(2,2,4)
-    plt.subplots_adjust(hspace=0.6, wspace=0.6)
-    xticks([]), yticks([])
-    plt.title("Sinal Discretizado Quantizado")
-    plt.axhline(0, color = 'black')
-    plt.axvline(0, color = 'black')
-    plt.ylabel('X[n]')
-    plt.xlabel('n')
-    plt.grid()
-    xj = np.round(signal1)
-    xR = 1/3 * xj
-    for i in range(0,len(sample1)):
-      plt.stem(sample1,xR)  
-      plt.plot(sample1[i],xR[i],'o')
-    
+ 
     
     #mostrando todos os 4 plot
     plt.show()
-    #limpando a cada iteração do programa
+    #limpando a cada iteracao do programa
     plt.clf()
     # OBS.: por estarem no mesmo subplot precisam estar na mesma subrotina
     
     print("\n")
 
 
-def startProgram():#função que inicia o programa, fica em looping mostrando o pequeno menu feito
+def startProgram():#funçãoo que inicia o programa, fica em looping mostrando o pequeno menu feito
     #apenas para fins calcular mais de uma vez,
 
     print("Digite as opções:\n[0] Para sair\n[1] Para inserir a equação do sinal analógico: ")
